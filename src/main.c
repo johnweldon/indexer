13dd9bee9412c94423ca24877629d4b5054e28cf
John Weldon <johnweldon4@gmail.com>
2019-12-28 20:18:55 -0700

    Insert file blobs

--- src/main.c
+++ src/main.c
@@ -8,6 +8,51 @@
 #include "main.h"
 #include "fnv/fnv.h"
 
+sqlite3 * DB = 0;
+
+static const size_t MAX_LEN = 1 << 30;
+static const char * const INIT_DB =
+    "CREATE TABLE IF NOT EXISTS files ("
+    "  path TEXT,"
+    "  hash INTEGER,"
+    "  size INTEGER,"
+    "  UNIQUE(path, hash, size));"
+    "CREATE TABLE IF NOT EXISTS blobs ("
+    "  hash INTEGER,"
+    "  size INTEGER,"
+    "  blob BLOB,"
+    "  UNIQUE(hash, size));"
+    "CREATE TABLE IF NOT EXISTS file_blobs ("
+    "  file_hash INTEGER,"
+    "  blob_hash INTEGER,"
+    "  ordinal INTEGER,"
+    "  UNIQUE(file_hash, blob_hash, ordinal));"
+    ;
+static const char * const ADD_FILE =
+    "INSERT OR IGNORE INTO files (path, hash, size)"
+    " VALUES(?, ?, ?)";
+static const char * const ADD_BLOB =
+    "INSERT OR IGNORE INTO blobs (hash, size, blob)"
+    " VALUES(?, ?, ?)";
+static const char * const ADD_FILE_BLOB =
+    "INSERT OR IGNORE INTO file_blobs (file_hash, blob_hash, ordinal)"
+    " VALUES(?, ?, ?)";
+
+struct node {
+    Fnv64_t hash;
+    int ordinal;
+    struct node * prev;
+};
+
+struct node * new_node(Fnv64_t hash, int ordinal, struct node * prev)
+{
+    struct node * n = malloc(sizeof(struct node));
+    n->hash = hash;
+    n->ordinal = ordinal;
+    n->prev = prev;
+    return n;
+}
+
 static int
 callback(void * notUsed, int argc, char ** argv, char ** azColName)
 {
@@ -23,10 +68,76 @@ callback(void * notUsed, int argc, char ** argv, char ** azColName)
     return 0;
 }
 
+void
+insert_blob(Fnv64_t hash, int size, const char * const buf)
+{
+    sqlite3_stmt * stmt;
+
+    if(SQLITE_OK == sqlite3_prepare_v2(DB, ADD_BLOB, -1, &stmt, NULL)) {
+        if(SQLITE_OK == sqlite3_bind_int64(stmt, 1, hash)) {
+            if(SQLITE_OK == sqlite3_bind_int(stmt, 2, size)) {
+                if(SQLITE_OK == sqlite3_bind_blob(stmt, 3, buf, size, SQLITE_STATIC)) {
+                    if(SQLITE_DONE == sqlite3_step(stmt)) {
+                        // SUCCESS
+                    }
+                }
+            }
+        }
+    }
+
+    sqlite3_finalize(stmt);
+}
+
+void
+insert_file( const char * const path, Fnv64_t hash, int size)
+{
+    sqlite3_stmt * stmt;
+
+    if(SQLITE_OK == sqlite3_prepare_v2(DB, ADD_FILE, -1, &stmt, NULL)) {
+        if(SQLITE_OK == sqlite3_bind_text(stmt, 1, path, strnlen(path, 1024),
+                                          SQLITE_STATIC)) {
+            if(SQLITE_OK == sqlite3_bind_int64(stmt, 2, hash)) {
+                if(SQLITE_OK == sqlite3_bind_int(stmt, 3, size)) {
+                    if(SQLITE_DONE == sqlite3_step(stmt)) {
+                        // SUCCESS
+                    }
+                }
+            }
+        }
+    }
+
+    sqlite3_finalize(stmt);
+}
+
+void
+insert_file_blob(Fnv64_t file_hash, Fnv64_t blob_hash, int ordinal)
+{
+    sqlite3_stmt * stmt;
+
+    if(SQLITE_OK == sqlite3_prepare_v2(DB, ADD_FILE_BLOB, -1, &stmt, NULL)) {
+        if(SQLITE_OK == sqlite3_bind_int64(stmt, 1, file_hash)) {
+            if(SQLITE_OK == sqlite3_bind_int64(stmt, 2, blob_hash)) {
+                if(SQLITE_OK == sqlite3_bind_int(stmt, 3, ordinal)) {
+                    if(SQLITE_DONE == sqlite3_step(stmt)) {
+                        // SUCCESS
+                    }
+                }
+            }
+        }
+    }
+
+    sqlite3_finalize(stmt);
+}
+
 unsigned long long int
 fnv_hash(const char * const fp, const double len)
 {
     FILE * fd = 0;
+    const size_t max = len < MAX_LEN ? len : MAX_LEN;
+    size_t read = 0;
+    short ordinal = 0;
+    Fnv64_t hash = FNV1A_64_INIT;
+    struct node * root = 0;
 
     if ((fd = fopen(fp, "rb")) == NULL) {
         fprintf(stderr, "Can't open file %s; %s\n", fp, strerror(errno));
@@ -34,17 +145,21 @@ fnv_hash(const char * const fp, const double len)
         return 0;
     };
 
-    const size_t max = len < MAX_LEN ? len : MAX_LEN;
-
     char * buf = malloc(max);
 
-    size_t read = 0;
-
-    Fnv64_t hash = FNV1A_64_INIT;
-
     while (0 != (read = fread(buf,  1, max, fd))) {
-        fprintf(stdout, "\t\t read %lu bytes\n", read);
+        Fnv64_t blob_hash = FNV1A_64_INIT;
+        blob_hash = fnv_64a_buf(buf, read, blob_hash);
+        insert_blob(blob_hash, read, buf);
+        root = new_node(blob_hash, ordinal++, root);
         hash = fnv_64a_buf(buf, read, hash);
+    }
+
+    insert_file(fp, hash, len);
+
+    while(0 != root) {
+        insert_file_blob(hash, root->hash, root->ordinal);
+        root = root->prev;
     }
 
     fclose(fd);
--- src/main.h
+++ src/main.h
@@ -1,17 +1,6 @@
 #ifndef _SRC_MAIN_
 #define _SRC_MAIN_
 
-#include <stdlib.h>
 
-#include "sqlite3.h"
-
-sqlite3 * DB = 0;
-
-static const size_t MAX_LEN = 1 << 30;
-static const char * const INIT_DB =
-    "CREATE TABLE IF NOT EXISTS files ("
-    "  hash TEXT,"
-    "  path TEXT,"
-    "  size INTEGER)";
 
 #endif /* _SRC_MAIN_ */
